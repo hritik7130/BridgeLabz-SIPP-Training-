c748291dec22a6c4b36c4fe549b0e22df6a0a103
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-06-13 09:18:48 +0530

    [Hritik]Add . Program to find sum of n natural numbers

---
+++ ControlFlow/SumOfNaturalNumber.java
@@ -0,0 +1,24 @@
+package ControlFlow;
+import java.util.*;
+
+public class SumOfNaturalNumber {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the value of n ");
+        int n=sc.nextInt();
+        int sumbyloop=0;
+        for(int i=1;i<=n;i++){
+            sumbyloop=sumbyloop+i;
+        }
+        int sumbyformula=n*(n+1)/2;
+
+        if(sumbyformula==sumbyloop){
+            System.out.println("The result was correct");
+        }
+        
+
+    
+    }
+
+    
+}
