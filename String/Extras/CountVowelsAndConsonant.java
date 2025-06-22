1615d14437d8319422a5ab71ce6a386fe9500f97
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-06-22 22:33:08 +0530

    origin[Hritik] Program to trim white spaces without using trim function

---
+++ String/Extras/CountVowelsAndConsonant.java
@@ -0,0 +1,23 @@
+package String.Extras;
+import java.util.*;
+
+public class CountVowelsAndConsonant {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc=new Scanner((System.in));
+    String str1=sc.nextLine();
+
+   
+
+    for(int i=0;i<str1.length();i++){
+        if(str1.charAt(i)>='A'&&str1.charAt(i)<='Z'){
+            
+        }
+
+    }}
+
+
+
+    
+}
---
+++ String/Extras/TextEdditingTool.java
@@ -0,0 +1,21 @@
+package String.Extras;
+import java.util.*;
+
+public class TextEdditingTool {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str1=sc.nextLine();
+      
+    }
+    public static String formatedString(String str){
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==';'||str.charAt(i)=='.'||str.charAt(i)=='?')
+            {  
+
+            }
+        }
+
+    }
+    
+}
